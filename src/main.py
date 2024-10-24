ed71c53e939c47c3530eb1a8165bb4761d86efef
Auluspersiusflaccus <felix.charron-ducharme.1@ulaval.ca>
2024-10-24 12:30:09 -0400

    C: finished Textnode class in textnode.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,9 @@
-print("hello world")
+import textnode
+
+def main():
+    dummy_node = textnode.TextNode("this is a text node", textnode.TextType.BOLD_TEXT, "https://www.boot.dev")
+    print(dummy_node)
+
+
+if __name__ == "__main__":
+    main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,16 +1,25 @@
 from enum import Enum
 
 class TextType(Enum):
-    NORMAL_TEXT = "normal text"
-    BOLD_TEXT = "bold text"
-    ITALIC_TEXT = "italic text"
-    CODE_TEXT = "code text"
+    NORMAL_TEXT = "normal"
+    BOLD_TEXT = "bold"
+    ITALIC_TEXT = "italic"
+    CODE_TEXT = "code"
     LINKS = "links"
     IMAGES = "images"
 
-class TextNode(text, text_type, url=None):
-   self.text = text
-   self.text_type = text_type
-   self.url = url
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
 
+    def __eq__(self, other):
+        if not isinstance(other, TextNode):
+            return False
+        if self.text == other.text and self.text_type == other.text_type and self.url == other.url: 
+            return True
+        return False
 
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
