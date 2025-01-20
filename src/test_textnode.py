3d469bd5809342a1cd59cc8c50d1b2c534a3fb62
Auluspersiusflaccus <felix.charron-ducharme.1@ulaval.ca>
2025-01-20 11:46:07 -0500

    E: wrote tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,6 @@
 import unittest
 
+
 from textnode import TextNode, TextType
 
 
@@ -8,7 +9,23 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
-
+    def test_eq2(self):
+        node = TextNode("132456", TextType.CODE)
+        node2 = TextNode("132456", TextType.CODE)
+        self.assertEqual(node, node2)
+    def test_noteq(self):
+        node = TextNode("112456", TextType.CODE)
+        node2 = TextNode("132456", TextType.CODE)
+        self.assertNotEqual(node, node2)
+    def test_true(self):
+        node = TextNode("This is a text node", TextType.BOLD, None)
+        self.assertTrue(node)
+    def test_noteq3(self):
+        node = TextNode("123456", TextType.CODE, "https://www.boot.dev/lessons/")
+        node2 = TextNode("132456", TextType.CODE, "https://www.boot.dev/lesson/")
+        self.assertNotEqual(node, node2)
+    def test_badinput(self):
+        self.assertRaises(TypeError, TextNode, "123")
 
 if __name__ == "__main__":
     unittest.main()
